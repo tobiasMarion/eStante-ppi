51c6d369fcf6401fb440b08bf427c319aa618d12
Tobias <tobiascm87@gmail.com>
2022-10-29 22:25:51 -0300

    Fininhing Home Page 🏡

---
+++ scripts/swiperSetup.js
@@ -0,0 +1,27 @@
+let slidesPerView
+
+if (innerWidth < 512) {
+    slidesPerView = 2
+} else if (innerWidth < 768) {
+    slidesPerView = 3
+} else if (innerWidth < 1024) {
+    slidesPerView = 4
+} else {
+    slidesPerView = 5
+}
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView,
+    spaceBetween: 30,
+    slidesPerGroup: slidesPerView,
+    loop: true,
+    loopFillGroupWithBlank: false,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
